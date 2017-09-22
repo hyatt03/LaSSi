dbdf994246f121ca0bf4dd3faffd79262a98e55e
Jonas Peter Hyatt <jph@dbc.dk>
2017-09-22 09:45:00 +0200

    plot parameter and prints

--- sim_entry.py
+++ sim_entry.py
@@ -142,7 +142,6 @@ def main():
     o = handle_arguments() # Getting options
     o = handle_constant_properties(o)
 
-
     print('Opening molecule')
     particles = handle_molecule_from_file(o)
 
