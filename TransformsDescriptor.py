d3089e22f02ab2173993449e7f50367d620e0def
Jonas Peter Hyatt <hyatt03@gmail.com>
2020-11-20 12:12:38 +0100

    Update TransformsDescriptor.py

--- TransformsDescriptor.py
+++ TransformsDescriptor.py
@@ -4,6 +4,6 @@ from tables import IsDescription, Float32Col, ComplexCol
 class TransformsDescriptor(IsDescription):
     energy = Float32Col(pos=1)
     frequency = Float32Col(pos=2)
-    I_xx = ComplexCol(pos=3)
-    I_yy = ComplexCol(pos=4)
-    I_zz = ComplexCol(pos=5)
+    I_xx = ComplexCol(pos=3, itemsize=16)
+    I_yy = ComplexCol(pos=4, itemsize=16)
+    I_zz = ComplexCol(pos=5, itemsize=16)
